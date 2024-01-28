d2e0b81a7602a8d6edf3db37774afe7c90f518a3
nero <devstory.co.kr@gmail.com>
2024-01-28 14:09:53 +0900

    Add free translation.

--- src/translator/google_translator.ts
+++ src/translator/google_translator.ts
@@ -8,14 +8,34 @@ export class GoogleTranslator implements Translator {
     this.config = config;
   }
 
-  async bulkTranslate(
+  public async bulkTranslate(
     query: string[],
-    targetLang: string
+    targetLang: string,
+    isPaid: boolean
   ): Promise<string[] | undefined> {
-    return await Promise.all(query.map((q) => this.translate(q, targetLang)));
+    return await Promise.all(
+      query.map((q) =>
+        isPaid
+          ? this.paidTranslate(q, targetLang)
+          : this.freeTranslate(q, targetLang)
+      )
+    );
   }
 
-  async translate(query: string, targetLang: string): Promise<string> {
+  public translate(
+    query: string,
+    targetLang: string,
+    isPaid: boolean
+  ): Promise<string> {
+    return isPaid
+      ? this.paidTranslate(query, targetLang)
+      : this.freeTranslate(query, targetLang);
+  }
+
+  private async paidTranslate(
+    query: string,
+    targetLang: string
+  ): Promise<string> {
     try {
       const apiKey = this.config.data.googleAPIKey;
       const source = this.config.data.fromLang.q;
@@ -31,6 +51,36 @@ export class GoogleTranslator implements Translator {
         const translatedText = json.data.translations[0].translatedText;
         // console.log(`${query} -> ${translatedText}`);
         return translatedText;
+      } else {
+        throw json.error;
+      }
+    } catch (e: any) {
+      console.error(e.message);
+      throw e.message;
+    }
+  }
+
+  private async freeTranslate(
+    query: string,
+    targetLang: string
+  ): Promise<string> {
+    try {
+      const source = this.config.data.fromLang.q;
+      const q = encodeURIComponent(query);
+      const res = await fetch(
+        `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${source}&tl=${targetLang}&dt=t&dt=bd&dj=1&source=icon&hl=${targetLang}&q=${q}`
+      );
+      const json: any = await res.json();
+      if (res.status === 200) {
+        if ("sentences" in json) {
+          let result: string = "";
+          for (const sentence of json.sentences) {
+            result += sentence.trans;
+          }
+          return result;
+        } else {
+          return json.dict[0].terms[0];
+        }
       } else {
         throw json.error;
       }
